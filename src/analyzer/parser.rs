42025455ceec4fc3c664e9eaf1102f81e18500eb
Anton Kudryavtsev <antonkudryavtsevdoem@gmail.com>
2023-03-22 14:12:47 +0300

    reverse polish notation for new lexer

--- src/analyzer/parser.rs
+++ src/analyzer/parser.rs
@@ -1,9 +1,75 @@
-use super::{syntax_tree::SyntaxTree, token::Token};
-use anyhow::Result;
+use std::collections::VecDeque;
+
+use super::{
+    syntax_tree::SyntaxTree,
+    token::{self, Token},
+};
+use anyhow::{anyhow, Result};
 
 pub fn build_tree<TokenIter>(token_stream: TokenIter) -> Result<SyntaxTree>
 where
     TokenIter: Iterator<Item = Token>,
 {
-    todo!()
+    let queue = reverse_polish(token_stream)?;
+
+    todo!("build tree from reverse polish notation")
+}
+
+pub fn reverse_polish<TokenIter>(token_stream: TokenIter) -> Result<VecDeque<Token>>
+where
+    TokenIter: Iterator<Item = Token>,
+{
+    let mut stack = VecDeque::<Token>::new();
+    let mut queue = VecDeque::<Token>::new();
+
+    for token in token_stream {
+        match token.kind {
+            token::Kind::Number(_) => queue.push_back(token),
+            token::Kind::Func(_) => stack.push_front(token),
+            token::Kind::Delimeter(token::Delim::FuncArgs) => queue.push_back(token),
+            token::Kind::Parenthesis(token::Paren::Open) => stack.push_front(token),
+
+            token::Kind::Parenthesis(token::Paren::Close) => {
+                while on_top(&stack, |t| t.kind != token::Kind::Parenthesis(token::Paren::Open)) {
+                    queue.push_back(stack.pop_front().unwrap());
+                }
+
+                if on_top(&stack, |t| t.kind == token::Kind::Parenthesis(token::Paren::Open)) {
+                    stack.pop_front(); // discard left parenthesis
+                } else {
+                    return Err(anyhow!("unmatched parenthesis in the token stream"));
+                }
+            }
+
+            token::Kind::BinaryOperator(_) | token::Kind::UnaryOperator(_) => {
+                while on_top(&stack, |t| {
+                    t.prec > token.prec || t.prec == token.prec && token.assoc == token::Associativity::Left
+                }) {
+                    queue.push_back(stack.pop_front().unwrap());
+                }
+                stack.push_front(token);
+            }
+
+            token::Kind::Delimeter(token::Delim::Comma) => {
+                while on_top(&stack, |t| {
+                    t.kind != token::Kind::Parenthesis(token::Paren::Open) && t.prec > token.prec
+                }) {
+                    queue.push_back(stack.pop_front().unwrap());
+                }
+            }
+        }
+    }
+
+    while !stack.is_empty() {
+        queue.push_back(stack.pop_front().unwrap());
+    }
+
+    return Ok(queue);
+}
+
+fn on_top<F>(stack: &VecDeque<Token>, condition: F) -> bool
+where
+    F: Fn(&Token) -> bool,
+{
+    stack.front().map_or(false, condition)
 }
--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,20 @@ use std::fs;
 
 fn main() -> Result<()> {
     let expr = fs::read_to_string("examples/expr7.rth").expect("failed to read file");
-    println!("Input Expression:\n{}", &expr);
+    println!("Input Expression:\n\t{}", &expr);
 
-    match analyzer::lexer::tokenize(expr.chars()) {
-        Ok(tokens) => {
-            for t in tokens {
-                print!("{:?} ", t.kind);
-            }
-        }
-        Err(err) => println!("Errors: {}", err),
+    let tokens = analyzer::lexer::tokenize(expr.chars())?;
+
+    println!("Tokens:");
+    for t in &tokens {
+        println!("\t{:?}", t.kind);
+    }
+
+    let rpn = analyzer::parser::reverse_polish(tokens.into_iter())?;
+
+    println!("Reverse Polish:");
+    for t in &rpn {
+        println!("\t{:?}", t.kind);
     }
 
     // https://cs.lmu.edu/~ray/notes/compilerarchitecture/
